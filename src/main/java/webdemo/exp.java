45ece8ca0475253d192d6a659a5c48ab94f5c90f
APARANJI <APARANJI@LAPTOP-NVIS5OT0>
2022-11-23 21:08:49 +0530

    This is a web demo

---
+++ src/main/java/webdemo/exp.java
@@ -0,0 +1,10 @@
+package webdemo;
+
+public class exp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Welcome to Devops web project!!!" );
+	}
+
+}
